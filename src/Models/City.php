dcefd50daa4cf3c9e6f1bbdb1328eb4c836f6141
Wajid Afaneh <afaneh262@gmail.com>
2018-12-16 11:59:26 +0200

    Create City.php

---
+++ src/Models/City.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TCG\Voyager\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Traits\HasRelationships;
+use TCG\Voyager\Traits\Translatable;
+use TCG\Voyager\Traits\Spatial;
+use TCG\Voyager\Traits\Resizable;
+
+class City extends Model
+{
+    use Translatable;
+    use Spatial;
+
+    protected $translatable = ['name'];
+
+    protected $guarded = [];
+
+    protected $spatial = ['coordinates'];
+}
