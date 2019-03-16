419c3c78179a7ca10cc85c3d130c8d98fbaa4b78
Issa Khalil <issa.mh.khalil@gmail.com>
2019-03-16 17:29:28 +0200

    Update app-footer.blade.php

--- resources/views/partials/app-footer.blade.php
+++ resources/views/partials/app-footer.blade.php
@@ -3,7 +3,7 @@
         @if (rand(1,100) == 100)
             <i class="voyager-rum-1"></i> {{ __('voyager::theme.footer_copyright2') }}
         @else
-            {!! __('voyager::theme.footer_copyright') !!} <a href="https://www.facebook.com/WajedAfaneh" target="_blank">Wajed Afaneh</a>
+            {!! __('voyager::theme.footer_copyright') !!}
         @endif
         @php $version = Voyager::getVersion(); @endphp
         @if (!empty($version))
